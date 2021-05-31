4d2b29a2ec8a65696c25d9fcf3c0818c0b859b04
Herobroto <84041378+Herobrot@users.noreply.github.com>
2021-05-31 11:09:09 -0500

    Update soup.js

--- soup.js
+++ soup.js
@@ -12,7 +12,7 @@ var app = new Vue({
 
     methods: {
         BuscarPel: async function() {
-            var Url = "http://www.omdbapi.com/?t="+this.Titu+"&apikey=9d86fa5c"; //Dirección de la pagian de la api+El Título de la película+La llave de la api
+            var Url = "https://www.omdbapi.com/?t="+this.Titu+"&apikey=9d86fa5c"; //Dirección de la pagian de la api+El Título de la película+La llave de la api
             const response = await fetch(Url);
             this.Peli= await response.json(); //la información de la pelicula es transformada en un array
             console.log(this.Peli);

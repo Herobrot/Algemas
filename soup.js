224a2123d62397a83e77621563608a1dcfc1e07d
Herobroto <84041378+Herobrot@users.noreply.github.com>
2021-05-30 23:57:08 -0500

    Update soup.js

--- soup.js
+++ soup.js
@@ -1,21 +1,20 @@
 var app = new Vue({
   el: '#app',
-  data: {
+  data: { //Variables para Vue
     //Jêshua Meza Reyes
       Open: [],
       Peli: [],
-      Titu: "green",
+      Titu: null,
       Result: null,
+      Img: null,
   },
     
-mounted(){
-  this.BuscarPel();  
-},
+
     methods: {
         BuscarPel: async function() {
-            var Url = "http://www.omdbapi.com/?t="+this.Titu+"&apikey=9d86fa5c";
+            var Url = "http://www.omdbapi.com/?t="+this.Titu+"&apikey=9d86fa5c"; //Dirección de la pagian de la api+El Título de la película+La llave de la api
             const response = await fetch(Url);
-            this.Peli= await response.json();
+            this.Peli= await response.json(); //la información de la pelicula es transformada en un array
             console.log(this.Peli);
         },
                  

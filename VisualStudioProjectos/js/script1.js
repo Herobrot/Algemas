b7c66c800d8ef9e0c06ab112c2a20ecda3bb3095
Herobroto <84041378+Herobrot@users.noreply.github.com>
2022-04-26 18:30:34 -0500

    Add files via upload

---
+++ VisualStudioProjectos/js/script1.js
@@ -0,0 +1,12 @@
+/*const slider = document.querySelector(".slider");
+let SectionSli = document.querySelectorAll(".img");
+let SectionSliUlt = SectionSli[SectionSli.length -1];
+
+const BtnAnte = document.querySelector("#Prev");
+const BtnDesp = document.querySelector("#Next");
+
+slider.insertAdjacentElement('afterbegin', SectionSliUlt);
+
+function Dere() {
+    let SectionSliFirst = document.querySelectorAll(".img")[0];
+} */
